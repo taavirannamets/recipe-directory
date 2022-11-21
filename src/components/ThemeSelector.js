6e8d381f7a7f62738961016916f4f04ed582b686
taavirannamets <taavirannamets@gmail.com>
2022-11-22 01:49:24 +0200

    added light & dark modes toggle

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,4 +1,5 @@
 import { useTheme } from '../hooks/useTheme'
+import modeIcon from '../assets/icons/mode-icon.svg'
 
 // Styles
 import './ThemeSelector.css'
@@ -6,10 +7,23 @@ import './ThemeSelector.css'
 const themeColors = ['var(--main)', 'var(--secondary)', 'var(--tertiary)']
 
 export default function ThemeSelector() {
-	const { changeColor } = useTheme()
+	const { changeColor, changeMode, mode } = useTheme()
+
+	const toggleMode = () => {
+		changeMode(mode === 'dark' ? 'light' : 'dark')
+	}
+	console.log(mode)
 
 	return (
 		<div className='theme-selector'>
+			<div className='mode-toggle'>
+				<img
+					onClick={toggleMode}
+					src={modeIcon}
+					alt='Dark/light toggle icon'
+					style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)' }}
+				/>
+			</div>
 			<div className='theme-buttons'>
 				{themeColors.map(color => (
 					<div
--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
-		<ThemeProvider>
-			<App />
-		</ThemeProvider>
-	</React.StrictMode>
+	// <React.StrictMode>
+	<ThemeProvider>
+		<App />
+	</ThemeProvider>
+	// </React.StrictMode>
 )
