b60350d5e3aa068e9853f5033f065e47d6100e34
taavirannamets <taavirannamets@gmail.com>
2022-11-21 14:09:48 +0200

    added ingredients field to the form

--- src/routes/create/index.js
+++ src/routes/create/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 // Styles
 import './Create.css'
@@ -7,10 +7,24 @@ export default function Create() {
 	const [title, setTitle] = useState('')
 	const [method, setMethod] = useState('')
 	const [cookingTime, setCookingTime] = useState('')
+	const [newIngredient, setNewIngredient] = useState('')
+	const [ingredients, setIngredients] = useState([])
+	const ingredientInput = useRef(null)
 
 	const handleSubmit = e => {
 		e.preventDefault()
-		console.log(title, method, cookingTime)
+		console.log(title, method, cookingTime, ingredients)
+	}
+
+	const handleAdd = e => {
+		e.preventDefault()
+		const ing = newIngredient.trim()
+
+		if (ing && !ingredients.includes(ing)) {
+			setIngredients(prevIngredients => [...prevIngredients, ing])
+		}
+		setNewIngredient('')
+		ingredientInput.current.focus()
 	}
 
 	return (
@@ -22,15 +36,34 @@ export default function Create() {
 					<span>Title</span>
 					<input
 						type='text'
-						onChange={e => {
-							setTitle(e.target.value)
-						}}
+						onChange={e => setTitle(e.target.value)}
 						value={title}
 						required
 					/>
 				</label>
 
-				{/* Ingredients here later */}
+				<label>
+					<span>Ingredients</span>
+					<div className='ingredients'>
+						<input
+							type='text'
+							onChange={e => setNewIngredient(e.target.value)}
+							value={newIngredient}
+							ref={ingredientInput}
+						/>
+						<button className='btn' onClick={handleAdd}>
+							Add
+						</button>
+					</div>
+				</label>
+				<p>
+					Ingredients added:{' '}
+					{ingredients.map(i => (
+						<em key={i} className='ingredients-added'>
+							{i}
+						</em>
+					))}
+				</p>
 
 				<label>
 					<span>Method</span>
