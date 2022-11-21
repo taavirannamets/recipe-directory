2d1f9f5252a7234e5a84338121e07bfceb224a52
taavirannamets <taavirannamets@gmail.com>
2022-11-21 23:28:49 +0200

    added searchbar with styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom'
 
+// Components
+import Searchbar from './Searchbar'
+
 // Styles
 import './Navbar.css'
 
@@ -10,6 +13,7 @@ export default function Navbar() {
 				<Link to='/' className='brand'>
 					<h1>Recipe Directory</h1>
 				</Link>
+				<Searchbar />
 				<Link to='/create'>Add new recipe</Link>
 			</nav>
 		</div>
