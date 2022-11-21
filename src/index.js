e0186a425b4a78cb71ddeaba9a155503d957fb68
taavirannamets <taavirannamets@gmail.com>
2022-11-21 23:57:25 +0200

    added theme context and provider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+// Context
+import { ThemeProvider } from './context/ThemeContext'
+
 // Styles
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 )
