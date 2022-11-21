27abee7b168194c7b080ead5a9e6e929e5441a90
taavirannamets <taavirannamets@gmail.com>
2022-11-21 13:33:26 +0200

    added a form for creating recipes, with styling

--- src/routes/create/index.js
+++ src/routes/create/index.js
@@ -1,6 +1,59 @@
+import { useState } from 'react'
+
 // Styles
 import './Create.css'
 
 export default function Create() {
-	return <div> Create</div>
+	const [title, setTitle] = useState('')
+	const [method, setMethod] = useState('')
+	const [cookingTime, setCookingTime] = useState('')
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		console.log(title, method, cookingTime)
+	}
+
+	return (
+		<div className='create'>
+			<h1 className='page-title'>Add a new recipe</h1>
+
+			<form onSubmit={handleSubmit}>
+				<label>
+					<span>Title</span>
+					<input
+						type='text'
+						onChange={e => {
+							setTitle(e.target.value)
+						}}
+						value={title}
+						required
+					/>
+				</label>
+
+				{/* Ingredients here later */}
+
+				<label>
+					<span>Method</span>
+					<textarea
+						onChange={e => setMethod(e.target.value)}
+						value={method}
+						required
+					/>
+				</label>
+				<label>
+					<span>Cooking time</span>
+					<div className='cooking-time'>
+						<input
+							type='number'
+							onChange={e => setCookingTime(e.target.value)}
+							value={cookingTime}
+							required
+						/>{' '}
+						<span className='input-add'>minutes</span>
+					</div>
+				</label>
+				<button className='btn'>Save recipe</button>
+			</form>
+		</div>
+	)
 }
